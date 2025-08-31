8ecd2665a06fac9da534a0a5be34a8d101fc4ca9
Óscar <oscar.iglsan@educa.jcyl.es>
2025-08-31 12:51:53 +0200

    preparacion

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Portafolio de Óscar de la Iglesia';
+export const SITE_DESCRIPTION = 'Aquí encontrarás mis proyectos, curriculum, servicios ... etc';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
