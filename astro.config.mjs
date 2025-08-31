5440a9b90d847eda3fb8ac493ef667ec8da6338b
Óscar <oscar.iglsan@educa.jcyl.es>
2025-08-31 17:53:22 +0200

    prep

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: 'https://o-isan.github.io',
+  site: 'https://o-isan.github.io/oisandev/',
   base: '/oisandev/',           // 👈 muy importante, se queda en '/' porque este repo es el "especial"
   output: 'static',
   integrations: [mdx(), sitemap(), tailwind()],
